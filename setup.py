cf32cf2458adf1db7cecb1e7e3beedbc48ac7f25
Kevin Jacobs <kevin91nl@gmail.com>
2017-06-17 17:11:09 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='scrape-ticker-symbols',
     packages=['tickersymbols'],
-    version='0.2',
+    version='0.3',
     description='Scrape all ticker symbols from a given URL.',
     author='Kevin Jacobs',
     author_email='mail@kevinjacobs.nl',
     url='https://github.com/kevin91nl/scrape-ticker-symbols',
-    download_url='https://github.com/kevin91nl/scrape-ticker-symbols/archive/0.1.tar.gz',
+    download_url='https://github.com/kevin91nl/scrape-ticker-symbols/archive/0.3.tar.gz',
     keywords=['ticker symbol', 'ticker', 'symbol', 'stock market', 'stock', 'scrape'],
     classifiers=[],
 )
